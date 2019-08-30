154d9bf2eaca01d2b79de4f4ba15873187dd7c03
Sebastian Mazza <sebastian@mazza.at>
2019-08-30 11:46:08 +0200

    [TASK] TYPO3 compatibility - make unsubscribe links possible in direct_mail newsletters (exclude dynamic parameters from cHash creation)

--- ext_localconf.php
+++ ext_localconf.php
@@ -13,3 +13,9 @@ if (!defined('TYPO3_MODE')) {
         'Subscription' => 'new, subscribe, confirm, unsubscribe, unsubscribeform, message',
     )
 );
+
+
+#tx_dmailsubscribe_fe%5Baction%5D=unsubscribe&tx_dmailsubscribe_fe%5BconfirmationCode%5D=bf7a7038&tx_dmailsubscribe_fe%5Bcontroller%5D=Subscription&tx_dmailsubscribe_fe%5BsubscriptionUid%5D=1282&cHash=ff945e417faf1db3f2322c98af7c3b85
+# do not generate cHash for newsletter link params
+$TYPO3_CONF_VARS['FE']['cacheHash']['excludedParameters'][] = 'tx_dmailsubscribe_fe[confirmationCode]';
+$TYPO3_CONF_VARS['FE']['cacheHash']['excludedParameters'][] = 'tx_dmailsubscribe_fe[subscriptionUid]';
